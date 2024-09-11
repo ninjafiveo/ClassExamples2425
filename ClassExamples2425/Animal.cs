d3a135e24ff6de0bd559198017bbce941b88942b
ninjafiveo <mjsekol@live.com>
2024-09-11 12:08:28 -0400

    Add project files.

---
+++ ClassExamples2425/Animal.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExamples2425
+{
+    internal class Animal
+    {   // The Zoo
+        // What properties do we need?
+        // Type of animal (Species), name, age, weight, height, region
+        // Let's set the properties. Getters and Setters...
+        public string AnimalSpecies { get; set; }
+        public string AnimalName { get; set;}
+        public int AnimalAge { get; set; }
+        public double AnimalWeight { get; set;}
+        public double AnimalHeight { get; set; }
+        public int AnimalSpeed {  get; set; }
+        public string AnimalRegion {  get; set; }
+
+        // Constructor
+        public Animal(string animalSpecies, string animalName, int animalAge, double animalWeight, double animalHeight, int animalSpeed, string animalRegions)
+        {
+            AnimalSpecies = animalSpecies;
+            AnimalName = animalName;
+            AnimalAge = animalAge;
+            AnimalWeight = animalWeight;
+            AnimalHeight = animalHeight;
+            AnimalSpeed = animalSpeed;
+            AnimalRegion = animalRegions;
+        }
+
+        // Method - Behaviour
+        public void Run()
+        {
+            Console.WriteLine($"{AnimalName} the {AnimalSpecies} breaks free from the zoo and runs away at {AnimalSpeed} MPH.");
+        }
+
+        public void Roll()
+        {
+
+        }
+    }
+}
---
+++ ClassExamples2425/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExamples2425
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal cat1 = new Animal("Feline", "Beelzebub", 1032, 120, 1.5, 25, "The Underworld");
+            Animal doggo1 = new Animal("God's Gift to Humans", "St. Mozzy", 5, 14, 1.5, 299, "The Promise Land");
+
+            Console.WriteLine(doggo1.AnimalName + " vs. " + cat1.AnimalName);
+            doggo1.Run();
+            cat1.Run();
+
+            Console.ReadKey();
+        }
+    }
+}
